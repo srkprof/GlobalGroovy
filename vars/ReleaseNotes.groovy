5204ac8ddbba71c0f4d67e362a97505f864bce6f
Sri Tannidi <sriramakrishna.tannidi@careerbuilder.com>
2020-10-22 11:43:50 -0500

    First commit

---
+++ vars/ReleaseNotes.groovy
@@ -0,0 +1,19 @@
+import java.io.*;
+import groovy.io.*;
+def call(Map config=[:])
+{
+ def dir = new File(pwd()); //pwd();
+    new File(dir.Path + '\\releasenotes.txt').withWriter('utf-8')
+    {
+      writer ->
+        dir.eachFileRecurse(FileType.ANY) { file ->
+        if(file.isDirectory())
+        {
+          writer.writeLine(file.name)
+        }
+        else
+        { 
+          writer.writeLine(file.name + '\t' + file.length())
+        }
+     }
+}
