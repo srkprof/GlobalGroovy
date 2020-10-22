ae4e1f68f8f50c9589ca9791ba2aef925153c1d2
Sri Tannidi <sriramakrishna.tannidi@careerbuilder.com>
2020-10-22 13:09:12 -0500

    Addig semi columns

--- vars/ReleaseNotes.groovy
+++ vars/ReleaseNotes.groovy
@@ -1,19 +1,21 @@
 import java.io.*;
 import groovy.io.*;
+
 def call(Map config=[:])
 {
  def dir = new File(pwd()); //pwd();
-    new File(dir.Path + '\\releasenotes.txt').withWriter('utf-8')
+    new File(dir.Path + '/releasenotes.txt').withWriter('utf-8')
     {
-      writer ->
-        dir.eachFileRecurse(FileType.ANY) { file ->
-        if(file.isDirectory())
-        {
-          writer.writeLine(file.name)
-        }
-        else
-        { 
-          writer.writeLine(file.name + '\t' + file.length())
-        }
-     }
+          writer ->
+            dir.eachFileRecurse(FileType.ANY) { file ->
+            if(file.isDirectory())
+            {
+              writer.writeLine(file.name);
+            }
+            else
+            { 
+              writer.writeLine(file.name + '\t' + file.length());
+            }
+         }
+    }
 }
