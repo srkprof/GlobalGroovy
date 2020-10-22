c0cb495a08e298e8ac62340c8ddb41de750665f3
Sri Tannidi <sriramakrishna.tannidi@careerbuilder.com>
2020-10-22 13:16:22 -0500

    correcting syntax

--- vars/ReleaseNotes.groovy
+++ vars/ReleaseNotes.groovy
@@ -4,7 +4,7 @@ import groovy.io.*;
 def call(Map config=[:])
 {
  def dir = new File(pwd()); //pwd();
-    new File(dir.Path + '/releasenotes.txt').withWriter('utf-8')
+    new File(dir.path + '/releasenotes.txt').withWriter('utf-8')
     {
           writer ->
             dir.eachFileRecurse(FileType.ANY) { file ->
